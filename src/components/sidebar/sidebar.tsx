772f2d138935c8af5a1281b372c0f2547c3cac86
Alfiandi Rizki <alfian.jw@gmail.com>
2025-04-27 22:42:28 +0700

    Fixing Side Bar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -5,21 +5,36 @@ import {
   SidebarGroup,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { Button } from "../ui/button";
+import { CgProfile } from "react-icons/cg";
+import { GoStack } from "react-icons/go";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { GoProjectRoadmap } from "react-icons/go";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader className="text-center"> CMS </SidebarHeader>
+      <SidebarHeader className="text-center text-lg font-bold">
+        Dashboard{" "}
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton asChild>
               <a href="#">
+                <CgProfile />
                 <span>Edit Profile</span>
               </a>
             </SidebarMenuButton>
@@ -27,6 +42,7 @@ export function AppSidebar() {
           <SidebarMenuItem>
             <SidebarMenuButton asChild>
               <a href="#">
+                <GoStack />
                 <span>Stack</span>
               </a>
             </SidebarMenuButton>
@@ -34,16 +50,54 @@ export function AppSidebar() {
           <SidebarMenuItem>
             <SidebarMenuButton asChild>
               <a href="#">
+                <MdOutlineWorkOutline />
                 <span>Work Experience</span>
               </a>
             </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuAction>
+                  <MoreHorizontal />
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="right" align="start">
+                <DropdownMenuItem>
+                  <span>List Work Experience</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Add Work Experience</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Update Work Experience</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
           <SidebarMenuItem>
             <SidebarMenuButton asChild>
               <a href="#">
+                <GoProjectRoadmap />
                 <span>Project</span>
               </a>
             </SidebarMenuButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuAction>
+                  <MoreHorizontal />
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="right" align="start">
+                <DropdownMenuItem>
+                  <span>List Project</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Add Project</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Update Project</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarGroup />
