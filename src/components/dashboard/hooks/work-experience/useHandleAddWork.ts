43848e2efc8bdcc00084b3157f49e27584a9c54f
Alfiandi Rizki <alfian.jw@gmail.com>
2025-05-02 03:33:43 +0700

    feat(work): create Post Raw Work Experience

---
+++ src/components/dashboard/hooks/work-experience/useHandleAddWork.ts
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { WorkExperience } from "../../types/work-experience";
+import { axiosInstance } from "@/config/axios";
+
+export default function useHandleAddWork() {
+  const { register, handleSubmit } = useForm<WorkExperience>();
+
+  const { mutateAsync: addWork, isPending: isPendingAddWork } = useMutation({
+    mutationKey: ["Add-Work"],
+    mutationFn: async (data: WorkExperience) => {
+      const response = await axiosInstance.post("/v1/work-experience", data);
+
+      return response.data;
+    },
+  });
+
+  const onSubmitAddWork = async (data: WorkExperience) => {
+    await addWork(data);
+  };
+
+  return { onSubmitAddWork, register, handleSubmit, isPendingAddWork };
+}
